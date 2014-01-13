bc25513007a9858ce3f7923a400152f38694b50b
cooper-j <james.cooper@epitech.eu>
2014-01-13 18:54:52 +0100

    tmp

--- MediaPlayer/View/MainWindow.xaml.cs
+++ MediaPlayer/View/MainWindow.xaml.cs
@@ -529,5 +529,35 @@ namespace MediaPlayer
             }
             this._fullScreen = !this._fullScreen;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (System.Security.Cryptography.MD5 md5Hash = System.Security.Cryptography.MD5.Create())
+            {
+                string hash = GetMd5Hash(md5Hash, "test");
+            }
+        }
+
+        static string GetMd5Hash(System.Security.Cryptography.MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
     }
 }
