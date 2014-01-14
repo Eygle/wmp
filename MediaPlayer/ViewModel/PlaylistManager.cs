1bcde75d7dbf0a7199de47aeea60c7237ee898ef
Matthieu Przybylski <matthieu.przybylski@gmail.com>
2014-01-14 17:34:35 +0100

    playlist manager désormais contenu dans mainwindow

--- MediaPlayer/ViewModel/PlaylistManager.cs
+++ MediaPlayer/ViewModel/PlaylistManager.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MediaPlayer.ViewModel
 {
     public class PlaylistManager
     {
-        private string[] _folders;
+        private List<string> _playlists;
 
         public PlaylistManager() 
         { 
+            this._playlists = new List<string>();
         }
 
         public void AddFolder(string name)
         {
         }
 
-        public void AddPlaylist(string folder, string name)
+        public bool AddPlaylist(string name)
         {
+            Regex r = new Regex("^[a-z|0-9|\\s]+$", RegexOptions.IgnoreCase);
+
+            if (this._playlists.Contains(name) || !r.Match(name).Success)
+                return (false);
+            this._playlists.Add(name); // TODO: think about the path
+            return (true);
         }
 
         public void AddFiles(string playlist, string[] path)
