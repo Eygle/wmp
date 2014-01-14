46d7f8f5d692bd0a432d9ec88a054283548c26e7
Matthieu Przybylski <matthieu.przybylski@gmail.com>
2014-01-14 14:37:50 +0100

    added a ContextMenu in TreeView

--- MediaPlayer/ViewModel/MediaFactory.cs
+++ MediaPlayer/ViewModel/MediaFactory.cs
@@ -9,6 +9,9 @@ namespace MediaPlayer.ViewModel
 {
     class MediaFactory
     {
+        private delegate IMedia CreateMediaDelegate(string pathname);
+        
+        //private Dictionary<string, CreateMediaDelegate> _allowedExt; // TODO: use this instead of the code below
         private string[] _allowedExt = { ".mp3", ".mp4", ".asf", ".3gp", ".3g2", ".asx", ".avi", ".jpg", ".jpeg", ".gif", ".bmp", ".png", ".wma", ".mov" };
         private string[] _videoExt = { ".mp4", ".asf", ".3gp", ".3g2", ".asx", ".avi", ".mov", ".wma"};
         private string[] _imageExt = { ".jpg", ".jpeg", ".gif", ".bmp", ".png" };
